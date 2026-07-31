99387ec69453a5fe01cfcec46f78f9104a81a75b
Mirllan <edilsonmirllan038@gmail.com>
2026-07-31 10:39:43 -0500

    proxy

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const app = express();
+
+const PORT = process.env.PORT;
+const AUTH_HEADERS = {
+    Authorization: `Token ${process.env.API_TOKEN}`,
+    "Content-Type": "application/json",
+};
+
+app.use(cors());
+
+app.get("/api/Bago/productos", async (req, res) => {
+    try {
+        const apiUrl = "https://botai.smartdataautomation.com/api_backend_ai/dinamic-db/report/119/productos_bago";
+
+        console.log(`Consultando API: ${apiUrl}`);
+
+        const response = await fetch(apiUrl, { headers: AUTH_HEADERS });
+        const data = await response.json();
+        res.json(data);
+    } catch (err) {
+        console.error("Error en el proxy productos Bago:", err);
+        res.status(500).json({ error: "Error al obtener datos de productos Bago" });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
